4a632ee725ad0b352ffbbfcb21c62f0b900758b6
Sarvar <sarvarismoilxojayev31@gmail.com>
2023-04-06 22:38:23 +0500

    first

--- src/Components/Episode.jsx
+++ src/Components/Episode.jsx
@@ -2,81 +2,104 @@ import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import axios from "axios";
 import "../Styles/Location.css";
-import { Link} from "react-router-dom";
-import '../Styles/Catalog.css'
+import { Link } from "react-router-dom";
+import "../Styles/Catalog.css";
+import Load from "./Load";
 
 export default function Location() {
-  const [post, setPost] = useState();
   const [load, setLoad] = useState(false);
+  let [lyrcs, setLyrcs] = useState(null);
   const [data, setData] = useState([]);
-  const [id, setId] = useState('')
+  const [item, setItem] = useState([]);
+  const [number, setNumber] = useState(1);
+  
 
-  useEffect(
-    (e) => {
-      async function app() {
-        setLoad(true);
-        let res = await axios.get(`https://rickandmortyapi.com/api/episode`);
-        setData(res.data.results);
-        setLoad(true);
-      }
-      app();
-    },
-    [post]
-  );
+  // let url = `https://rickandmortyapi.com/api/location/1`;
 
+  // useEffect(() => {
+  //   fetch(url).then((res) => res.json()).then((data) => {
+  //     let api = data.residents[0]
+  //     data.residents[0]
+  //     fetch(api).then((res) => res.json()).then((data) => {
+  //       setItem(data);
+  //     })
+  //   })
+  // }, [URL]);
 
-  console.log(data);
+  let url = `https://rickandmortyapi.com/api/episode/${number}`;
+ 
+  useEffect(() => {
+    (async function () {
+      const data = await fetch(url).then((res) => res.json());
+      setItem(data);
 
-  function change(e) {
-   
+      const x = await Promise.all(
+        data.characters.map((item) => {
+          return fetch(item).then((res) => res.json());
+        })
+      );
+      setLoad(true)
+      setData(x);
+    })();
+  }, [url]);
 
-        console.log(id);
-  }
+  
+
 
 
   return (
     <div>
       <Navbar />
+      {load ? lyrcs : <Load />}
+      <div className="banner">
+        <span className="text-primary title d-block"> <span className="text-dark">Location  :  </span>{item.name}</span>
+        <span className="discription">Air Date : {item.air_date}</span>
+      </div>
       <div className="container total d-flex">
         <div className="b1">
-        <select
-          className="cars form-select select"
-          aria-label="Disabled select example"
-        >
-          <option disabled selected>
-            choose
-          </option>
-          {data.map((i) => {
-            return <option value={id} onChange={e => setId(e.target.value)} onClick={change}>{i.id}</option>;
-          })}
-        </select>
+          <select
+            className="cars form-select select"
+            aria-label="Disabled select example"
+            onChange={(e) => setNumber(e.target.value)}
+          >
+            {data.map((i, index) => {
+                return (
+                  <option>{index + 1}</option>
+                )
+            })}
+          </select>
         </div>
         <div className="b2 d-flex">
-        {data.map((e) => {
-          const active = () => {
-            if (e.status === "Alive") {
-              return "green";
-            } else if (e.status === "Dead") {
-              return "dead";
-            } else if (e.status === "unknown") {
-              return "uncown";
-            }
-          };
-          return (
-            <div className="catalog" key={crypto.randomUUID()}>
-              <img src={e.image} alt="" className="img-item" width="260px" />
-              <div className="inf">
-                <span className={active()}>{e.status}</span>
-                <span className="name h2 d-block">{e.name}</span>
-                <span className="location d-block mt-4">Last Location</span>
-                <span className="d-block h5 mt-1">{e.dimension}</span>
-                <Link className="link text-decoration-none btn btn-primary" to={`/${e.id}`}>
-                  More
-                </Link>
+          {data.map((e) => {
+            const active = () => {
+              if (e.status === "Alive") {
+                return "green";
+              } else if (e.status === "Dead") {
+                return "dead";
+              } else if (e.status === "unknown") {
+                return "uncown";
+              }
+            };
+            return (
+              <div className="catalog" key={crypto.randomUUID()}>
+                <img src={e.image} alt="" className="img-item" width="260px" />
+                <div className="inf">
+                  <span className={active()}>{e.status}</span>
+                  <span className="name h2 d-block">{e.name}</span>
+                  <span className="location d-block mt-4">Last Location</span>
+                  <span className="d-block h5 mt-1">{e.dimension}</span>
+                  <Link
+                    className="link text-decoration-none btn btn-primary"
+                    to={`/${e.id}`}
+                  >
+                    More
+                  </Link>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+
+          
         </div>
       </div>
     </div>
--- src/Components/Location.jsx
+++ src/Components/Location.jsx
@@ -2,73 +2,120 @@ import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import axios from "axios";
 import "../Styles/Location.css";
-import { Link} from "react-router-dom";
-import '../Styles/Catalog.css'
+import { Link } from "react-router-dom";
+import "../Styles/Catalog.css";
+import Load from "./Load";
 
 export default function Location() {
-  const [post, setPost] = useState();
   const [load, setLoad] = useState(false);
   const [data, setData] = useState([]);
+  const [item, setItem] = useState([]);
+  let [lyrcs, setLyrcs] = useState(null);
+  let [number, setNumber] = useState(1);
+  
 
-  useEffect(
-    (e) => {
-      async function app() {
-        setLoad(true);
-        let res = await axios.get(`https://rickandmortyapi.com/api/location`);
-        setData(res.data.results);
-        setLoad(true);
-      }
-      app();
-    },
-    [post]
-  );
+  // let url = `https://rickandmortyapi.com/api/location/1`;
 
-  console.log(data);
+  // useEffect(() => {
+  //   fetch(url).then((res) => res.json()).then((data) => {
+  //     let api = data.residents[0]
+  //     data.residents[0]
+  //     fetch(api).then((res) => res.json()).then((data) => {
+  //       setItem(data);
+  //     })
+  //   })
+  // }, [URL]);
+
+  let url = `https://rickandmortyapi.com/api/location/${number}`;
+ 
+  useEffect(() => {
+    (async function () {
+      const data = await fetch(url).then((res) => res.json());
+      setItem(data);
+
+      const x = await Promise.all(
+        data.residents.map((item) => {
+          return fetch(item).then((res) => res.json());
+        })
+      );
+      setLoad(true)
+      setData(x);
+    })();
+  }, [url]);
+
+  
+
 
 
   return (
     <div>
       <Navbar />
+      {load ? lyrcs : <Load />}
+      <div className="banner">
+        <span className="text-primary title d-block"> <span className="text-dark">Location  :  </span>{item.name}</span>
+        <span className="discription">Dimension:  {item.dimension}</span>
+        <span className="planet">Type : {item.type}</span>
+      </div>
       <div className="container total d-flex">
         <div className="b1">
-        <select
-          className="cars form-select select"
-          aria-label="Disabled select example"
-        >
-          <option disabled selected>
-            choose
-          </option>
-          {data.map((i) => {
-            return <option value={i.id}>{i.id}</option>;
-          })}
-        </select>
+          <select
+            className="cars form-select select"
+            aria-label="Disabled select example"
+            onChange={(e) => setNumber(e.target.value)}
+          >      
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                  <option>6</option>
+                  <option>7</option>
+                  <option>8</option>
+                  <option>9</option>
+                  <option>10</option>
+                  <option>11</option>
+                  <option>12</option>
+                  <option>13</option>
+                  <option>14</option>
+                  <option>15</option>
+                  <option>16</option>
+                  <option>17</option>
+                  <option>18</option>
+                  <option>19</option>
+                  <option>20</option>
+          </select>
         </div>
         <div className="b2 d-flex">
-        {data.map((e) => {
-          const active = () => {
-            if (e.status === "Alive") {
-              return "green";
-            } else if (e.status === "Dead") {
-              return "dead";
-            } else if (e.status === "unknown") {
-              return "uncown";
-            }
-          };
-          return (
-            <div className="catalog" key={crypto.randomUUID()}>
-              <img src={e.image} alt="" className="img-item" width="260px" />
-              <div className="inf">
-                <span className={active()}>{e.status}</span>
-                <span className="name h2 d-block">{e.name}</span>
-                <span className="location d-block mt-4">Last Location</span>
-                <span className="d-block h5 mt-1">{e.dimension}</span>
-                <Link className="link text-decoration-none btn btn-primary" to={`/${e.id}`}>
-                  More
-                </Link>
+          {data.map((e) => {
+            const active = () => {
+              if (e.status === "Alive") {
+                return "green";
+              } else if (e.status === "Dead") {
+                return "dead";
+              } else if (e.status === "unknown") {
+                return "uncown";
+              }
+            };
+            return (
+              <div className="catalog" key={crypto.randomUUID()}>
+                <img src={e.image} alt="" className="img-item" width="260px" />
+                <div className="inf">
+                  <span className={active()}>{e.status}</span>
+                  <span className="name h2 d-block">{e.name}</span>
+                  <span className="location d-block mt-4">Last Location</span>
+                  <span className="d-block h5 mt-1">{e.dimension}</span>
+                  <Link
+                    className="link text-decoration-none btn btn-primary"
+                    to={`/${e.id}`}
+                  >
+                    More
+                  </Link>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+
+          
         </div>
       </div>
     </div>
